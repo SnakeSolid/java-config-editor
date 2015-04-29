51439aa003030c63c10fcc09f51767648eb530d5
snakesolid <none@none>
2015-04-29 19:21:21 +0600

    + extracted abstract appender class

---
+++ src/main/java/ru/sname/config/worker/util/AbstractAppender.java
@@ -0,0 +1,34 @@
+package ru.sname.config.worker.util;
+
+import java.util.regex.Matcher;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+
+public abstract class AbstractAppender implements Runnable {
+
+	protected static final int MAX_DOCUMENT_SIZE = 8192;
+
+	protected final StyledDocument document;
+
+	public AbstractAppender(StyledDocument document) {
+		this.document = document;
+	}
+
+	protected void trimToSize() throws BadLocationException {
+		int length = document.getLength();
+
+		if (length > MAX_DOCUMENT_SIZE) {
+			int trimStart = length - MAX_DOCUMENT_SIZE;
+			String part = document.getText(trimStart, MAX_DOCUMENT_SIZE);
+			Matcher matcher = Patterns.LINE.matcher(part);
+
+			if (matcher.find()) {
+				document.remove(0, trimStart + matcher.end());
+			} else {
+				document.remove(0, trimStart);
+			}
+		}
+	}
+
+}
--- src/main/java/ru/sname/config/worker/util/MessageAppender.java
+++ src/main/java/ru/sname/config/worker/util/MessageAppender.java
@@ -2,7 +2,6 @@ package ru.sname.config.worker.util;
 
 import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.regex.Matcher;
 
 import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledDocument;
@@ -10,12 +9,11 @@ import javax.swing.text.StyledDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MessageAppender implements Runnable {
+public class MessageAppender extends AbstractAppender {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(MessageAppender.class);
 
-	private static final int MAX_DOCUMENT_SIZE = 8192;
 	private static final DateFormat DATE_FORMATTER;
 
 	static {
@@ -23,45 +21,32 @@ public class MessageAppender implements Runnable {
 				DateFormat.MEDIUM);
 	}
 
-	private final StyledDocument document;
 	private final String message;
 
 	public MessageAppender(StyledDocument document, String message) {
-		this.document = document;
+		super(document);
+
 		this.message = message;
 	}
 
 	@Override
 	public void run() {
-		try {
-			execute();
-		} catch (BadLocationException e) {
-			logger.warn("Can not append log line", e.getMessage());
-		}
-	}
-
-	private void execute() throws BadLocationException {
-		int length = document.getLength();
-
-		if (length > MAX_DOCUMENT_SIZE) {
-			int trimStart = length - MAX_DOCUMENT_SIZE;
-			String part = document.getText(trimStart, length);
-			Matcher matcher = Patterns.LINE.matcher(part);
-
-			if (matcher.find(trimStart)) {
-				document.remove(0, matcher.end());
-			} else {
-				document.remove(0, length);
-			}
-		}
-
 		String dateText = DATE_FORMATTER.format(Calendar.getInstance()
 				.getTime());
 
-		document.insertString(document.getLength(), dateText, Attributes.STRONG);
-		document.insertString(document.getLength(), ": ", Attributes.LIGHT);
-		document.insertString(document.getLength(), message, Attributes.DEFAULT);
-		document.insertString(document.getLength(), "\n", Attributes.DEFAULT);
+		try {
+			document.insertString(document.getLength(), dateText,
+					Attributes.STRONG);
+			document.insertString(document.getLength(), ": ", Attributes.LIGHT);
+			document.insertString(document.getLength(), message,
+					Attributes.DEFAULT);
+			document.insertString(document.getLength(), "\n",
+					Attributes.DEFAULT);
+
+			trimToSize();
+		} catch (BadLocationException e) {
+			logger.warn("Can not append log line", e.getMessage());
+		}
 	}
 
 }
